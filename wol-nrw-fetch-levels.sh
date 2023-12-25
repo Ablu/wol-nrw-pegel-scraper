fdf820a943338916667e2f4785f1409a69af237a
Erik Schilling <git@ablu.org>
2023-12-25 11:41:14 +0100

    Initial version of script
    JavaScript array is embedded into the website, so we just slice it out
    and treat it as JSON.

---
+++ wol-nrw-fetch-levels.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+date=$(date --iso-8601=minutes)
+folder=$(echo "$date" | awk -FT '{print $1}')
+mkdir "$folder"
+(echo '{'; curl https://www.wol-nrw.de/aktuelle-messwerte/ | sed -n '/var data/,/;/p'|tail -n+2 | head -n-1; echo '}') | jq > "$folder/$date.json"
