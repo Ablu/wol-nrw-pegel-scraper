9a1f97f0ca5da3c358f95f15e7b20e01e90c45c4
Erik Schilling <git@ablu.org>
2023-12-25 12:01:20 +0100

    Write to XDG_DATA_HOME dir

--- wol-nrw-fetch-levels.sh
+++ wol-nrw-fetch-levels.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 date=$(date --iso-8601=minutes)
-folder=$(echo "$date" | awk -FT '{print $1}')
+folder="$HOME/.local/share/wol-nrw-scraper/$(echo "$date" | awk -FT '{print $1}')"
 mkdir -p "$folder"
 (echo '{'; curl --silent https://www.wol-nrw.de/aktuelle-messwerte/ | sed -n '/var data/,/;/p'|tail -n+2 | head -n-1; echo '}') | jq > "$folder/$date.json"
