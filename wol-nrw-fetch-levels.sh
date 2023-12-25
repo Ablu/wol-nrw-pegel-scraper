7c3ba9ac0ea3275280be7de3ee34e13b16fbf77e
Erik Schilling <git@ablu.org>
2023-12-25 11:44:31 +0100

    Call curl with --silent

--- wol-nrw-fetch-levels.sh
+++ wol-nrw-fetch-levels.sh
@@ -3,4 +3,4 @@
 date=$(date --iso-8601=minutes)
 folder=$(echo "$date" | awk -FT '{print $1}')
 mkdir -p "$folder"
-(echo '{'; curl https://www.wol-nrw.de/aktuelle-messwerte/ | sed -n '/var data/,/;/p'|tail -n+2 | head -n-1; echo '}') | jq > "$folder/$date.json"
+(echo '{'; curl --silent https://www.wol-nrw.de/aktuelle-messwerte/ | sed -n '/var data/,/;/p'|tail -n+2 | head -n-1; echo '}') | jq > "$folder/$date.json"
