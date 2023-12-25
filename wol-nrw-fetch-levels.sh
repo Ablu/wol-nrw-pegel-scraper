79987bfe37e31ae786c63bad5cccb9c8d9c58f36
Erik Schilling <git@ablu.org>
2023-12-25 11:43:45 +0100

    Do not error if dir exists

--- wol-nrw-fetch-levels.sh
+++ wol-nrw-fetch-levels.sh
@@ -2,5 +2,5 @@
 
 date=$(date --iso-8601=minutes)
 folder=$(echo "$date" | awk -FT '{print $1}')
-mkdir "$folder"
+mkdir -p "$folder"
 (echo '{'; curl https://www.wol-nrw.de/aktuelle-messwerte/ | sed -n '/var data/,/;/p'|tail -n+2 | head -n-1; echo '}') | jq > "$folder/$date.json"
